c898c0924180291d047c50b31e916ea8b52dba3c
raulrocha2 <raul.rocha2@gmail.com>
2020-03-12 18:30:41 -0300

    creat home_pahe.html, add in settings, alter views and urls.py

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import home_page #The .views because class views.py this in the same folder urls.py  
 
 urlpatterns = [
+    path('', home_page), 
     path('admin/', admin.site.urls),
 ]
