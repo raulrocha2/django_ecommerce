26bc31445b5927f8b82b1b1583b43e109174ec51
raulrocha2 <raul.rocha2@gmail.com>
2020-03-17 12:14:20 -0300

    Create context able title an content to home_page, about_page and contact_page, create and set src/template/about/view.html, src/template/contat/view.html and updates src/template/home_page.html to use context variables

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home_page #The .views because class views.py this in the same folder urls.py  
+from .views import home_page, about_page, contact_page
+ #The .views because class views.py this in the same folder urls.py  
 
 urlpatterns = [
     path('', home_page), 
+    path('about/', about_page),
+    path('contact/', contact_page),
     path('admin/', admin.site.urls),
 ]
