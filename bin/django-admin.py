ed448ad51bf824ca5876b96de04b64084c76541a
raulrocha2 <raul.rocha2@gmail.com>
2020-03-28 16:52:14 -0300

    new venv in project

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/raul_mint/Documents/curso_python/c_django/django_ecommerce/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
