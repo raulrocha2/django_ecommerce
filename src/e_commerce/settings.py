281205ce281c1b43c634c7c4cb80251290ce3444
raulrocha2 <raul.rocha2@gmail.com>
2020-03-28 17:57:28 -0300

    insert MEDIA_URL in src/e-commerce/settings.py

--- src/e_commerce/settings.py
+++ src/e_commerce/settings.py
@@ -123,3 +123,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_local")
 ]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
